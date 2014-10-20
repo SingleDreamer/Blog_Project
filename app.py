880f8b222c365c0382b58627d03b44c88ebffaf3
Jessica Ng <jessica.hl.ng5@gmail.com>
2014-10-20 18:38:12 -0400

    <name> feature, other stuff

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request#, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     newpost = request.args.get ("new_post", None)
-    if newpost == "New Post":
-        return redirect (url_for('new_post'))
+    title = request.args.get ("title", None)
+    author = request.args.get ("author", None)
+    post = request.args.get ("post", None)
+    if newpost == "Post":
+        text =  title + "<br>" + author + "<br>" + post
+        return render_template ("index.html", posts = text)
     else:
-        return render_template ("index.html")
+        return render_template ("index.html", posts = "")
 
-@app.route("/new_post")
-def new_post():
-    return "yolo"
+@app.route("/<title>")
+def new_post(title):
+    return title + "yolo"
     
 if __name__ == "__main__":
     app.debug = True
-    app.run()
-        #host="0.0.0.0",port=8888)
+    app.run(host="0.0.0.0",port=8888)
