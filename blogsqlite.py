eca2fba20359384cb5e349a6818646bb2b9dc885
Nathaniel Brassell <nathaniel303@gmail.com>
2014-10-21 11:22:39 -0400

    Update blogsqlite.py

--- blogsqlite.py
+++ blogsqlite.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("blog.db")
 
 p = """create table posts(post_id integer, post_title text, post_author text, post_content text); 
-create table comments(comment_id integer, comment_content text, comment_author texr,p_id integer);"""
+create table comments(comment_id integer, comment_content text, comment_author text,p_id integer);"""
 
 c = conn.cursor()
 c.execute(p)
