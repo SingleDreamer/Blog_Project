74be44a9ad0816491ebf717cf9238de86c5c707f
Nathaniel Brassell <nathaniel303@gmail.com>
2014-10-22 11:49:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request#, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+#x=0
 conn = sqlite3.connect("blog.db")
-q = "create table posts(title text)"
-'''c=conn.cursor()
-c.execute(q)
-'''
+p ="create table posts(post_id integer, post_title text, post_author text, post_content text);"
+comm = "create table comments(comment_id integer, comment_content text, comment_author text,p_id integer);"
+
+c = conn.cursor()
+#c.execute(p)
+#c.execute(comm)
+conn.commit();
 
 @app.route("/")
 def home():
@@ -17,15 +21,24 @@ def home():
     #make lists and play around with nested loops and loops.index
     if newpost == "Post":
         link =  '/'+ title + ''
-
-        #q = "Insert Into posts Values(" + title +");"
-        #c.execute(q)
-        #conn.commit()
-        return render_template ("index.html", 
+        conn = sqlite3.connect("blog.db")
+        c = conn.cursor()
+        q = "Insert Into posts Values(1,'" + title + "','" + author + "','" + post + "');"
+        #x = x + 1
+        s = "Select post_title From posts"
+        t = "Select post_author From postxs"
+        u = "Select post_content From posts"
+        c.execute(q)
+        title_list = c.execute(s)
+        author_list = c.execute(t)
+        post_list = c.execute(u)            
+        conn.commit()
+        return render_template ("index.html",
+                                #ignore link
                                 link = link,
-                                title = title,
-                                author = author,
-                                post = post)
+                                title = title_list,
+                                author = author_list,
+                                post = post_list)
     else:
         return render_template ("index.html")
 
