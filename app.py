5c8bdb97f94b53247a875e36421733a15872b8a7
Jessica Ng <jessica.hl.ng5@gmail.com>
2014-10-23 04:24:38 -0400

    fixed main page

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request#, redirect, url_for
 import sqlite3
 
+
 app = Flask(__name__)
 #x=0
-conn = sqlite3.connect("blog.db")
-p ="create table posts(post_id integer, post_title text, post_author text, post_content text);"
-comm = "create table comments(comment_id integer, comment_content text, comment_author text,p_id integer);"
+#conn = sqlite3.connect("blog.db")
+#p ="create table posts(post_id integer, post_title text, post_author text, post_content text);"
+#comm = "create table comments(comment_id integer, comment_content text, comment_author text,p_id integer);"
 
-c = conn.cursor()
+#c = conn.cursor()
 #c.execute(p)
 #c.execute(comm)
-conn.commit();
+#conn.commit();
 
 @app.route("/")
 def home():
@@ -19,28 +20,35 @@ def home():
     author = request.args.get ("author", None)
     post = request.args.get ("post", None)
     #make lists and play around with nested loops and loops.index
+    conn = sqlite3.connect("blog.db")
+    c = conn.cursor()
     if newpost == "Post":
-        link =  '/'+ title + ''
-        conn = sqlite3.connect("blog.db")
-        c = conn.cursor()
-        q = "Insert Into posts Values(1,'" + title + "','" + author + "','" + post + "');"
-        #x = x + 1
-        s = "Select post_title From posts"
-        t = "Select post_author From postxs"
-        u = "Select post_content From posts"
+        q = "Insert Into posts Values('" + title + "','" + author + "','" + post + "');"
         c.execute(q)
-        title_list = c.execute(s)
-        author_list = c.execute(t)
-        post_list = c.execute(u)            
-        conn.commit()
-        return render_template ("index.html",
-                                #ignore link
-                                link = link,
-                                title = title_list,
-                                author = author_list,
-                                post = post_list)
-    else:
-        return render_template ("index.html")
+        #x = x + 1
+    #link =  '/'+ title + ''
+    s = "Select post_title From posts"
+    t = "Select post_author From posts"
+    u = "Select post_content From posts"
+    title_list = []
+    author_list = []
+    post_list = []
+    titles = c.execute(s)
+    title_list = [(str(x)[3:])[:-3] for x in titles]
+    authors= c.execute(t)
+    author_list = [(str(x)[3:])[:-3] for x in authors]
+    posts= c.execute(u)            
+    post_list = [(str(x)[3:])[:-3] for x in posts]
+    conn.commit()
+    print title_list
+    print author_list
+    print post_list
+    return render_template ("index.html",
+                            #ignore link
+                            #link = link,
+                            title_list = title_list,
+                            author_list = author_list,
+                            post_list = post_list)
 
 @app.route("/<title>")
 ##check if title is unique
